bec1592fc2030cc366af0b12e165733a48309d63
MudasirStanikzay <ahmad.mudasir.stanikzay@iths.se>
2025-06-02 21:19:01 +0200

    Add application

---
+++ src/main/java/se/iths/app/MovieApplication.java
@@ -0,0 +1,9 @@
+package se.iths.app;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MovieApplication extends Application {
+
+}
