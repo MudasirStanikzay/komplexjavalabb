912413a79c24ca2d33186ecdbf9727cfa1b2ccbb
MudasirStanikzay <ahmad.mudasir.stanikzay@iths.se>
2025-06-02 21:12:47 +0200

    Getter & Setter

--- src/main/java/se/iths/entity/Movie.java
+++ src/main/java/se/iths/entity/Movie.java
@@ -1,8 +1,13 @@
 package se.iths.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Setter
+@Getter
 @Entity
 public class Movie {
 
@@ -40,51 +45,4 @@ public class Movie {
 
     // Getters och setters
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
 }
